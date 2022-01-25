35cd488513f25e82c14b682cf9427bf0cd111071
radoslavpetkov <98400839+radoslavpetkov@users.noreply.github.com>
2022-01-25 15:26:57 +0200

    Create config.php

---
+++ inc/config.php
@@ -0,0 +1,18 @@
+<?php
+define('SITE', TRUE);
+
+//BAse URL
+define('BASE_URL','/cms-simple');
+//Site name
+define('SITE_NAME', 'CMS проект');
+
+// File base path
+define('SITE_ROOT', 'C://xampp//htdocs//cms-simple//');
+
+
+
+//databese
+define('DB_SERVER','localhost');
+define('DB_USER', 'root');
+define('DB_PASS','');
+define('DB_DATABASE', 'cms_simle');
